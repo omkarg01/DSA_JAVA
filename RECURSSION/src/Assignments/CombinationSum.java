6df953f8916a1d28cacd8a3d8c890e0dd7afd3b7
Omkar Gujja <omkargujja01@gmail.com>
2021-11-17 21:44:38 +0530

    CombinationSum

---
+++ RECURSSION/src/Assignments/CombinationSum.java
@@ -0,0 +1,33 @@
+package Assignments;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * CombinationSum
+ */
+public class CombinationSum {
+    public static void main(String[] args) {
+        List<List<Integer>> list = combinationSum(new int[]{2, 3 , 6 , 7}, 7);
+        System.out.println(list);
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(candidates, 0, target, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void backtrack(int[] cand, int start, int target, List<Integer> list, List<List<Integer>> result) {
+        if (target < 0)
+            return;
+        if (target == 0)
+            result.add(new ArrayList<>(list));
+        for (int i = start; i < cand.length; i++) {
+            list.add(cand[i]);
+            backtrack(cand, i, target - cand[i], list, result);
+            list.remove(list.size() - 1);
+        } 
+    }
+
+}
